60985ec0482ac117fa8b3578062fb9948ccd4f42
Dujet <duje0807@gmail.com>
2024-08-19 19:51:28 +0200

    random debug visuals change

--- Assets/Scripts/Weapon_Raycast.cs
+++ Assets/Scripts/Weapon_Raycast.cs
@@ -8,10 +8,10 @@ public class Weapon_Raycast : Weapon
     {
         RaycastHit hit;
         Vector3 direction = target.position - transform.position;
-        Debug.DrawLine(transform.position, transform.position + direction.normalized * AttackRange, Color.red, 2.0f);
+        Debug.DrawLine(transform.position, transform.position + direction.normalized * AttackRange, Color.red, 1.0f);
         if (Physics.Raycast(transform.position, direction, out hit, AttackRange))
         {
-            Debug.DrawLine(transform.position, target.position, Color.green, 2.0f);
+            Debug.DrawLine(transform.position, hit.point, Color.green, 2.0f);
             Health targetHealth = hit.transform.GetComponent<Health>();
             if (targetHealth != null)
             {

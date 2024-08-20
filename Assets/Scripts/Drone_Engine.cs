1084ac384c74015579399c700f8bb696cde38d4b
Dujet <duje0807@gmail.com>
2024-08-20 19:18:25 +0200

    add t90 unit prefab and navmesh

--- Assets/Scripts/Drone_Engine.cs
+++ Assets/Scripts/Drone_Engine.cs
@@ -21,7 +21,7 @@ public class Drone_Engine : MonoBehaviour, IEngine
         upVec.x = 0f;
         upVec.z = 0f;
         float diff = 1 - upVec.magnitude;
-        float finalDiff = diff * Physics.gravity.magnitude;
+        float finalDiff = diff * Physics.gravity.magnitude; // added force to counter gravity while pitching/rolling
 
         Vector3 engineForce = Vector3.zero;
         engineForce = transform.up * ((rb.mass * Physics.gravity.magnitude) + finalDiff + input.ThrottleInput * maxPower) / 4f;

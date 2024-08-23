601d8ab09d4c12a09b9b0bb7af6c251a90a81401
Dujet <duje0807@gmail.com>
2024-08-23 17:39:59 +0200

    feat: spawn assault drone on target click and switch to it

---
+++ Assets/UnitHighlight.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitHighlight : MonoBehaviour
+{
+    [SerializeField] private Material _highlightMaterial;
+    [SerializeField] private Camera _camera;
+    private Material _defaultMaterial;
+    private Renderer _renderer;
+    private DroneCamera _otherDroneCamera;
+
+    void Update() {
+        if (_renderer == null) return;
+        //DrawSquare();
+    }
+
+    public void Init(Renderer renderer, DroneCamera otherDroneCamera) {
+        _renderer = renderer;
+        _defaultMaterial = _renderer.material;
+        _otherDroneCamera = otherDroneCamera;
+        Highlight();
+    }
+
+    public void Highlight()
+    {
+        _defaultMaterial = _renderer.material;
+        _renderer.material = _highlightMaterial;
+    }
+
+    public void RemoveHighlight()
+    {
+        if (_renderer == null) return;
+        _renderer.material = _defaultMaterial;
+    }
+
+    private void OnDisable()
+    {
+        RemoveHighlight();
+        _otherDroneCamera.EnableCamera();
+    }
+
+    private void OnDestroy()
+    {
+        RemoveHighlight();
+        _otherDroneCamera.EnableCamera();
+    }
+
+    // draw a UI square around the target unit
+    void OnDrawGizmos() {
+        if (_renderer == null) return;
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(_renderer.bounds.center, _renderer.bounds.size*2);
+    }
+}
--- Assets/UnitSpotter.cs
+++ Assets/UnitSpotter.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class UnitSpotter : MonoBehaviour
 {
@@ -8,6 +9,13 @@ public class UnitSpotter : MonoBehaviour
     private List<Transform> _visibleTargets = new List<Transform>();
     [SerializeField] private bool _showDebugGizmos = true;
     [SerializeField] private Camera _camera;
+    [SerializeField] private string _targetTag = "WARSAW";
+    [SerializeField] private UnitHighlight _assaultDronePrefab;
+    private DroneCamera _droneCamera;
+
+    void Awake() {
+        _droneCamera = GetComponent<DroneCamera>();
+    }
 
 
     void Start()
@@ -19,6 +27,23 @@ public class UnitSpotter : MonoBehaviour
     void Update()
     {
         _fieldOfView.viewAngle = _camera.fieldOfView;
+
+        if (Input.GetMouseButtonDown(0)) {
+            Ray ray = _camera.ScreenPointToRay(new Vector3(Screen.width/2, Screen.height/2, 0));
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit)) {
+                if (hit.collider.CompareTag(_targetTag)) {
+                    //Transform target = hit.collider.transform;
+                    //if (_visibleTargets.Contains(target)) {
+                        UnitHighlight unitHighlight = Instantiate(_assaultDronePrefab, transform.position - Vector3.up*5, Quaternion.identity);
+                        unitHighlight.Init(hit.transform.GetComponentInChildren<Renderer>(), _droneCamera);
+                        _droneCamera.DisableCamera();
+                        GetComponent<PlayerInput>().enabled = false;
+                        GetComponent<Drone_Inputs>().enabled = false;
+                    //}
+                }
+            }
+        }
     }
 
     void OnDrawGizmos() {
@@ -27,5 +52,11 @@ public class UnitSpotter : MonoBehaviour
         foreach (Transform target in _visibleTargets) {
             Gizmos.DrawCube(target.position, Vector3.one*3);
         }
+    }
+
+    public void EnableCamera() {
+        _droneCamera.EnableCamera();
+        GetComponent<PlayerInput>().enabled = false;
+        GetComponent<Drone_Inputs>().enabled = true;
     }
 }

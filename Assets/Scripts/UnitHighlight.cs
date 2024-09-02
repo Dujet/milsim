17b39cb4cca6db48fdee124bcebf05645adbec76
Dujet <duje0807@gmail.com>
2024-09-02 17:17:29 +0200

    code cleanup

--- Assets/Scripts/UnitHighlight.cs
+++ Assets/Scripts/UnitHighlight.cs
@@ -5,56 +5,15 @@ using UnityEngine.UI;
 
 public class UnitHighlight : MonoBehaviour
 {
-    [SerializeField] private RawImage _targetMarkerPrefab;
-    [SerializeField] private Camera _camera;
-    private Transform _target;
     private UnitSpotter _otherDroneCamera;
-    private RawImage _targetMarker;
-    [SerializeField] private Canvas _canvas;
 
+    // TODO: rename class to something more appropriate
     public void Init(Transform target, UnitSpotter otherDroneCamera) {
-        _target = target;
         _otherDroneCamera = otherDroneCamera;
-
-        return; // debug
-
-        _targetMarker = Instantiate(_targetMarkerPrefab, _canvas.transform);
-        _targetMarker.color = Color.red;
-        
-        Canvas markerCanvas = _targetMarker.GetComponent<Canvas>();
-        if (markerCanvas == null) {
-            markerCanvas = _targetMarker.gameObject.AddComponent<Canvas>();
-        }
-        markerCanvas.overrideSorting = true;
-        markerCanvas.sortingOrder = 2;
-    }
-
-    void Awake() {
-        if (_canvas == null) _canvas = FindObjectOfType<Canvas>();
-    }
-
-    void Update() {
-        if (_target == null) return;
-
-        return; // debug
-        // Check if target is behind camera
-        Vector3 direction = (_target.position - Camera.main.transform.position).normalized;
-        bool isBehind = Vector3.Dot(direction, Camera.main.transform.forward) <= 0;
-        _targetMarker.enabled = !isBehind;
-
-        Vector3 screenPos = _camera.WorldToScreenPoint(_target.position);
-        _targetMarker.transform.position = screenPos;
-    }
-
-
-    private void OnDisable()
-    {
-        _otherDroneCamera.EnableCamera();
     }
 
     private void OnDestroy()
     {
         _otherDroneCamera.EnableCamera();
-        Destroy(_targetMarker.gameObject);
     }
 }

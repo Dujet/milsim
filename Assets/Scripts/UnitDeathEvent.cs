8aca7d84c44be42c5c81bbcd61fbf4311b0b7bc7
Dujet <duje0807@gmail.com>
2024-09-04 18:57:39 +0200

    tanks now burn and smoke when dead

--- Assets/Scripts/UnitDeathEvent.cs
+++ Assets/Scripts/UnitDeathEvent.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class UnitDeathEvent : DeathEvent
 {
+    [SerializeField] private GameObject _deathEffect;
+    [SerializeField] private Transform _deathEffectSpawnPoint;
     public override void OnDeath()
     {
         AIStateManager _aism = GetComponent<AIStateManager>();
         _aism.ChangeState(new UnitDeathState(_aism));
+
+        if (_deathEffect != null && _deathEffectSpawnPoint != null)
+        {
+            Instantiate(_deathEffect, _deathEffectSpawnPoint.position, _deathEffectSpawnPoint.rotation);
+        }
     }
 }
